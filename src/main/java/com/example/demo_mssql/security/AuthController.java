5ede8fcfabec22ee09246f96d0408e5bac613b98
pawan <pawan41281@gmail.com>
2025-04-24 21:21:07 +0530

    update

--- src/main/java/com/example/demo_mssql/security/AuthController.java
+++ src/main/java/com/example/demo_mssql/security/AuthController.java
@@ -1,96 +1,94 @@
 package com.example.demo_mssql.security;
 
-import java.util.HashSet;
-import java.util.Set;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import jakarta.validation.Valid;
+//import lombok.AllArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-
-//@CrossOrigin(origins = "*", maxAge = 3600)
-@AllArgsConstructor
-@RestController
-@RequestMapping("/api/auth")
+//@AllArgsConstructor
+//@RestController
+//@RequestMapping("/api/auth")
 public class AuthController {
 
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private RoleRepository roleRepository;
-
-	@Autowired
-	private AuthService authService;
-
-	@Autowired
-	private PasswordEncoder encoder;
-
-	// Build Login REST API
-	@PostMapping("/login")
-	public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto) {
-		String token = authService.login(loginDto);
-
-		JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
-		jwtAuthResponse.setAccessToken(token);
-
-		return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
-	}
-
-	// Create New User
-	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already exists!"));
-		}
-
-		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already exists!"));
-		}
-
-		// Create new user's account
-		User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
-				encoder.encode(signUpRequest.getPassword()));
-
-		Set<String> strRoles = signUpRequest.getRole();
-		Set<Role> roles = new HashSet<>();
-
-		if (strRoles == null) {
-			Role userRole = roleRepository.findByName("ROLE_USER");
-			if (userRole == null)
-				new RuntimeException("Error: role is not found in database.");
-			roles.add(userRole);
-					
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "ADMIN":
-					Role adminRole = roleRepository.findByName("ROLE_ADMIN");
-					if (adminRole == null)
-						new RuntimeException("Error: " + role + " role is not found in database.");
-					roles.add(adminRole);
-					break;
-
-				default:
-					Role userRole = roleRepository.findByName(role);
-					roles.add(userRole);
-				}
-			});
-
-		}
-
-		user.setRoles(roles);
-		userRepository.save(user);
-
-		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-	}
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	private RoleRepository roleRepository;
+//
+//	@Autowired
+//	private AuthService authService;
+//
+//	@Autowired
+//	private PasswordEncoder encoder;
+//
+//	// Build Login REST API
+//	@PostMapping("/login")
+//	public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto) {
+//		String token = authService.login(loginDto);
+//
+//		JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+//		jwtAuthResponse.setAccessToken(token);
+//
+//		return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
+//	}
+//
+//	// Create New User
+//	@PostMapping("/signup")
+//	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+//		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+//			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already exists!"));
+//		}
+//
+//		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+//			return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already exists!"));
+//		}
+//
+//		// Create new user's account
+//		User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
+//				encoder.encode(signUpRequest.getPassword()));
+//
+//		Set<String> strRoles = signUpRequest.getRole();
+//		Set<Role> roles = new HashSet<>();
+//
+//		if (strRoles == null) {
+//			Role userRole = roleRepository.findByName("ROLE_USER");
+//			if (userRole == null)
+//				new RuntimeException("Error: role is not found in database.");
+//			roles.add(userRole);
+//					
+//		} else {
+//			strRoles.forEach(role -> {
+//				switch (role) {
+//				case "ADMIN":
+//					Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+//					if (adminRole == null)
+//						new RuntimeException("Error: " + role + " role is not found in database.");
+//					roles.add(adminRole);
+//					break;
+//
+//				default:
+//					Role userRole = roleRepository.findByName(role);
+//					roles.add(userRole);
+//				}
+//			});
+//
+//		}
+//
+//		user.setRoles(roles);
+//		userRepository.save(user);
+//
+//		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+//	}
 
 }
